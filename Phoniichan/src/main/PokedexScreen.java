56633dddf04445b5f48d8b0baa75a2d9ee01474e
Student8 <Student8@10.8.33.156>
2017-02-02 14:00:39 -0500

    Created PokedexScreen. All other screens should extend this.

--- Phoniichan/src/main/PokedexScreen.java
+++ Phoniichan/src/main/PokedexScreen.java
@@ -2,11 +2,15 @@ package main;
 
 import java.util.ArrayList;
 
+import guiPractice.components.Button;
 import guiPractice.components.ClickableScreen;
 import guiPractice.components.Visible;
 
 public class PokedexScreen extends ClickableScreen {
 
+	private static Button toHome;
+	private static Button togglePower;
+	
 	public PokedexScreen(int width, int height) {
 		super(width, height);
 		// TODO Auto-generated constructor stub

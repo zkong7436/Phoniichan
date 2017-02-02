086f242be8019dc1714b0317df1b0ab82d81a9a6
Student8 <Student8@10.8.33.172>
2017-02-02 13:08:27 -0500

    created Pokedex class and main package

---
+++ Phoniichan/src/main/Pokedex.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package main;
+
+import guiPractice.GuiApplication;
+
+/**
+ * @author Student8
+ *
+ */
+public class Pokedex extends GuiApplication {
+
+	/**
+	 * 
+	 */
+	public Pokedex() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see guiPractice.GuiApplication#initScreen()
+	 */
+	@Override
+	protected void initScreen() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void main(String[] args){
+		
+	}
+
+}

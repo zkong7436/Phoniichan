00f2fa748478e013c27f5ca550072d996776034d
Student8 <Student8@10.8.33.156>
2017-02-06 13:00:26 -0500

    PokedexScreen 1.0 complete!

--- Phoniichan/src/main/PokedexScreen.java
+++ Phoniichan/src/main/PokedexScreen.java
@@ -2,24 +2,58 @@ package main;
 
 import java.util.ArrayList;
 
+import guiPractice.components.Action;
 import guiPractice.components.Button;
 import guiPractice.components.ClickableScreen;
 import guiPractice.components.Visible;
 
 public class PokedexScreen extends ClickableScreen {
 
-	private static Button toHome;
-	private static Button togglePower;
+	public static Button toHome;
+	public static Button togglePower;
+	
+	public static boolean locked;
+	public static boolean on;
+	
+	public static HomeScreen home;
+	public static LockScreen lock;
+	
+	private static int w;
+	private static int h;
 	
 	public PokedexScreen(int width, int height) {
 		super(width, height);
-		// TODO Auto-generated constructor stub
+		w = width;
+		h = height;
+		home = new HomeScreen(w,h);
 	}
 
 	@Override
 	public void initAllObjects(ArrayList<Visible> arg0) {
-		// TODO Auto-generated method stub
+		int buttonWidth = w/8;
+		toHome = new Button(w/8,h/2+h/16, buttonWidth, buttonWidth, null, null, new Action(){
 
+			@Override
+			public void act() {
+				if(on && !locked){
+					Pokedex.game.setScreen(home);
+				}
+			}
+			
+		});
+		togglePower = new Button(7*w/8,h/2+h/16, buttonWidth, buttonWidth, null, null, new Action(){
+
+			@Override
+			public void act() {
+				if(on){
+					on = false;
+				}else{
+					on = true;
+					Pokedex.game.setScreen(lock);
+				}
+			}
+			
+		});
 	}
 
 }

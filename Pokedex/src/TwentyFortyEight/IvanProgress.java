74c7f17f505d18138e4f4e8bdfce0c00e6d11e2b
Student 8 <Student 8@10.8.33.101>
2017-02-07 13:59:51 -0500

    setBoardMethod??

--- Pokedex/src/TwentyFortyEight/IvanProgress.java
+++ Pokedex/src/TwentyFortyEight/IvanProgress.java
@@ -1,9 +1,11 @@
 package TwentyFortyEight;
-
+import TwentyFortyEight.JimmyCondition;
 public class IvanProgress {
-
+	private int Score;
+	private int points;
 	public IvanProgress() {
-		// TODO Auto-generated constructor stub
+		points = JimmyCondition.getNewSq();
 	}
+	
 
 }
--- Pokedex/src/TwentyFortyEight/JosephBoard.java
+++ Pokedex/src/TwentyFortyEight/JosephBoard.java
@@ -11,12 +11,24 @@ import guiPractice.components.Component;
 
 public class JosephBoard extends Component implements BoardInterface{
 	private int[][] board = new int [4][4];
+	//private int sq1,sq2,sq3,sq4,sq5,sq6,sq7,sq8,sq9,sq10,sq11,sq12,sq13,sq14,sq15,s16;
+	private static int[]checkBoard = new int[16];
 	
 	public JosephBoard(int x, int y, int w, int h) {
 		super(x, y, w, h);
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	
+	//set board will put the numbers in the 2d array into a 1d array so we can then use some kind of sort/method to
+	//combine them
+	private static void setBoard(int[][] arr){
+		for (int row = 0,i = 0; row < arr.length; row++,i++) {
+			for (int col = 0; col < arr.length; col++) {
+				checkBoard[i] = arr[row][col];
+			}
+		}
+	}
 	@Override
 	public void update(Graphics2D g) {
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);

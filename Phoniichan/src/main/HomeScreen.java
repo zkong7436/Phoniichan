895a8c7f6a87c718b1d6a87140e86c6074dddf8d
Student8 <Student8@10.8.33.156>
2017-02-08 13:59:35 -0500

    ...

--- Phoniichan/src/main/HomeScreen.java
+++ Phoniichan/src/main/HomeScreen.java
@@ -2,10 +2,13 @@ package main;
 
 import java.util.ArrayList;
 
+import guiPractice.components.Button;
 import guiPractice.components.Visible;
 
 public class HomeScreen extends PokedexScreen {
 
+	private ArrayList<Button> appLink;
+	
 	public HomeScreen(int width, int height) {
 		super(width, height);
 		// TODO Auto-generated constructor stub
@@ -18,3 +21,5 @@ public class HomeScreen extends PokedexScreen {
 	}
 
 }
+//how many tiles? 20 (4x5)
+//need class AppLink!

8d15f6a849da4d1e8bef9bdbdc35bdda63137154
Jimmy <Jimmy@Jimmy-HP.home>
2017-02-13 23:08:32 -0500

    xd

--- Pokedex/src/TwentyFortyEight/JimmyTest.java
+++ Pokedex/src/TwentyFortyEight/JimmyTest.java
@@ -6,71 +6,65 @@ public class JimmyTest {
 	public static Scanner in = new Scanner(System.in);
 	static int[][] game;
 	static int temp;
-	
+
 	public static void main(String[] args) {
 		game = new int[4][4];
 		int x = 0;
 		for (int i = 0; i < game.length; i++) {
 			for (int j = 0; j < game[i].length; j++) {
 				game[i][j] = x;
-				
+
 			}
 		}
+
+		game[0][3] = 2;
+		game[1][3] = 2;
 	
-		game[3][0] = 2;
-		game[2][0] = 2;
-		
-		
-		printgame(game);
+		printGame(game);
 		playGame();
 	}
-	
-	
-	
-	
+
 	private static void playGame() {
+	
 		System.out.println("Enter a direction to slide the tile.");
 		String input = in.nextLine();
-		if (input .equals("d")) {
-			for (int i = 0; i < game.length; i++) {
-				for (int j = 0; j < game[i].length; j++) {
-					
-					if(j+1 < 4) {
-						game[i][j] = game[i][j+1] ;
-					}
-//					game[i][j] = temp;
-//					if (temp != 0) {
-//						game[i][j] = game[i][j+1];
-//					}
-				}
-			}
-		
+		if (input.equals("a")) {
+			  for(int i = 0; i < game.length; i++){
+			        int[] row = new int[4];
+			        for(int j = 0; j < game[i].length; j++){
+
+			            if (game[i][j] != 0)
+			            {
+			                temp = 0;
+			                while(row[temp] != 0) {
+			                    temp++;
+			                }
+			                row[temp] = game[i][j];
+			            }
+
+			        }
+			        game[i] = row;
+			    }
+			    printGame(game);
+			
 		}
-		
-		
-		
+
+	}
+	
+	
+	private static void moveTiles() {
+		// TODO Auto-generated method stub
 		
 	}
 
-
-
-
-
-
-
-
-
-
-
-
-	private static void printgame(int[][] game) {
-		for(int[] row : game) {
+	private static void printGame(int[][] game) {
+		for (int[] row : game) {
 			for (int i : row) {
-	            System.out.print(i);
-	            System.out.print("\t");
-	        }
-	        System.out.println();
+				System.out.print(i);
+				System.out.print("\t");
+			}
+			System.out.println();
 
-        }
+		}
 	}
 }

d588e6fcff0245fe27f9c8305557fabed6d033d8
IVAN <IVAN@192.168.1.2>
2017-02-13 20:08:26 -0500

    asdasd

--- Pokedex/src/TwentyFortyEight/MyDemo.java
+++ Pokedex/src/TwentyFortyEight/MyDemo.java
@@ -15,7 +15,6 @@ import guiPractice.components.Component;
  *
  */
 public class MyDemo extends Component {
-	int[][]a = new int[4][4];
 	public MyDemo(int x, int y, int w, int h) {
 		super(x, y, w, h);
 		// TODO Auto-generated constructor stub
@@ -27,37 +26,38 @@ public class MyDemo extends Component {
 
 	@Override
 	public void update(Graphics2D g) {
+		int[][]a = new int[4][4];
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setColor(Color.black);
 		int height = 40,width = 40;
 		for (int row = 0; row < a.length; row++) {
 			for (int col = 0; col < a.length; col++) {
 				g.drawRect(col*width,row*height, width, height);
-				if(a[row][col]== 0){
-
-					
-				}
+//				if(a[row][col]== 0){
+//
+//					
+//				}
 			}
 		}
 		
 	}
 
-	public void start(){
-		ArrayList<Integer> Nrow = new ArrayList<Integer>();
-		ArrayList<Integer> Ncol = new ArrayList<Integer>();
-		for (int row = 0; row < a.length; row++) {
-			for (int col= 0; col < a.length; col++) {
-				if(a[row][col] == 0){
-					Nrow.add(row);
-					Nrow.add(col);
-				}
-			}
-			int expo = (int) ((1 * Math.random()) + 1); // (0 or 1) + 1;
-			// 
-			// 2 ^1 = 2; 2^2 = 4;  it will random 
-		}
-		
-	}
+//	public void start(){
+//		ArrayList<Integer> Nrow = new ArrayList<Integer>();
+//		ArrayList<Integer> Ncol = new ArrayList<Integer>();
+//		for (int row = 0; row < a.length; row++) {
+//			for (int col= 0; col < a.length; col++) {
+//				if(a[row][col] == 0){
+//					Nrow.add(row);
+//					Nrow.add(col);
+//				}
+//			}
+//			int expo = (int) ((1 * Math.random()) + 1); // (0 or 1) + 1;
+//			// 
+//			// 2 ^1 = 2; 2^2 = 4;  it will random 
+//		}
+//		
+//	}
 	
 
 }

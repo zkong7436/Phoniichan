65a41798543e6a2cd2fb235b3c370500af89b57a
Student8 <Student8@10.8.33.161>
2017-02-14 12:48:22 -0500

    ?

--- Pokedex/src/memory/KevinScoreboard.java
+++ Pokedex/src/memory/KevinScoreboard.java
@@ -2,6 +2,7 @@ package memory;
 
 public class KevinScoreboard
 {
+	private int lives;
 	private int combo;
 	private int level;
 	private int currentScore;
@@ -9,6 +10,7 @@ public class KevinScoreboard
 	{
 		combo = 1;
 		level = 1;
+		lives = 3;
 		currentScore = 0;
 	}
 	
@@ -17,17 +19,17 @@ public class KevinScoreboard
 		if(correct == true)
 		{
 			combo = +1;
-			currentScore = +addScore(combo, level);
+			currentScore =+ addScore(combo, level);
 		}
 		else
 		{
 			combo = 1;
+			lives -= 1;
 		}
 		return currentScore;
 	} 
 	private int addScore(int combo, int level)
 	{
 		return combo * level;
-		
 	}
 }

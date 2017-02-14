7cebb99cdc9120d16190ea8d62f35094a010c77b
Student8 <Student8@10.8.33.156>
2017-02-14 13:51:27 -0500

    LockScreen v0.0.3

--- Pokedex/src/main/LockScreen.java
+++ Pokedex/src/main/LockScreen.java
@@ -12,6 +12,8 @@ public class LockScreen extends PokedexScreen {
 
 	private Button[][] buttons;
 	private Button enter;
+	private Button zero;
+	private Button backspace;
 	private String corCode = "022716";
 	private String code = "";
 	private TextLabel codeLabel;
@@ -24,24 +26,42 @@ public class LockScreen extends PokedexScreen {
 	@Override
 	public void initRemainingItems(ArrayList<Visible> viewObjects) {
 		buttons = new Button[3][3];
-		int buttonFactor = 100;
+		int buttonFactor = 70;
 		for(int i = 0; i< buttons.length; i++){
 			//figure out how to position each button
 			for(int j = 0; j < buttons[i].length; j++){
 				int a = i;
 				int b = j;
-				buttons[i][j] = new Button(370+i*buttonFactor,130+j*buttonFactor,buttonFactor,buttonFactor,(i+j+1+i*2)+"",new Color(0,0,0,1),new Action(){
+				buttons[i][j] = new Button(420+i*buttonFactor, 130+j*buttonFactor, buttonFactor, buttonFactor, (i+j+1+i*2)+"", new Color(0,0,0,0), new Action(){
 	
 					@Override
 					public void act() {
 						code += buttons[a][b].getText();
-						codeLabel.update();
+						codeLabel.setText(code);
 					}
 					
 				});
 				viewObjects.add(buttons[i][j]);			
 			}
 		}
+		zero = new Button(420, 130+3*buttonFactor, buttonFactor, buttonFactor, "0", new Color(0,0,0,0), new Action(){
+			
+			@Override
+			public void act() {
+				code += zero.getText();
+				codeLabel.setText(code);
+			}
+			
+		});
+		backspace = new Button(420+buttonFactor, 130+3*buttonFactor, buttonFactor*2, buttonFactor, "Backspace", new Color(0,0,0,0), new Action(){
+
+			@Override
+			public void act() {
+				code = code.substring(0, code.length()-1);
+				codeLabel.setText(code);
+			}
+			
+		});
 		enter = new Button(55,230,70,70,"Enter",new Color(0,0,0,0),new Action(){
 
 			@Override
@@ -53,7 +73,9 @@ public class LockScreen extends PokedexScreen {
 			}
 			
 		});
-		codeLabel = new TextLabel(370, 90, buttonFactor*3, buttonFactor*3, corCode);
+		codeLabel = new TextLabel(370, 90, buttonFactor*3, 25, corCode);
+		viewObjects.add(backspace);
+		viewObjects.add(zero);
 		viewObjects.add(enter);
 		viewObjects.add(codeLabel);
 	}

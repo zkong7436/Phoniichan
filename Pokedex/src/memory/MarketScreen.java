4e1d75500d24d099727982068556ed5dbfc9cc69
huangtoby <thuang7616@bths.edu>
2017-02-14 13:40:42 -0500

    Added more classes

--- Pokedex/src/memory/MarketScreen.java
+++ Pokedex/src/memory/MarketScreen.java
@@ -1,14 +1,54 @@
 package memory;
 
-public class MarketScreen {
+import java.util.ArrayList;
 
-	public MarketScreen() {
-		// TODO Auto-generated constructor stub
+import guiPractice.components.ClickableScreen;
+import guiPractice.components.Graphic;
+import guiPractice.components.TextLabel;
+import guiPractice.components.Visible;
+
+public class MarketScreen extends ClickableScreen implements Runnable{
+	
+	private TextLabel label;
+	private ButtonInterfaceFulton[] buttons;
+	private Graphic picture;
+	private int abraCaught;
+
+	public MarketScreen(int width, int height) {
+		super(width, height);
+		Thread app = new Thread(this);
+		app.start();
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	@Override
+	public void initAllObjects(ArrayList<Visible> viewObjects) {
+		addButtons(viewObjects);
+		
+		label = new TextLabel(20, 20, 760, 40, "AbraMarket");
+		
+		viewObjects.add(label);
+		
+	}
+	
+	private void addButtons(ArrayList<Visible> viewObjects) {
+		String[] names = {"Potion", "Potion2", "Potion3", "Potion4", "Potion5", "Potion6"};
+		String[] description = {"","","","","",""};
+		int numberOfButtons = 6;
+		buttons = new ButtonInterfaceFulton[numberOfButtons];
+		for(int i = 0; i <numberOfButtons; i++ ){
+			buttons[i] = getAButton();
+			buttons[i].setName(names[i]);
+		}
+	}
 
+	private ButtonInterfaceFulton getAButton() {
+		return new ButtonToby;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }

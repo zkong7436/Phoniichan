c3f81a1dd6ba3e181bdd95991f4f5cf408150f18
Student8 <Student8@10.8.33.156>
2017-02-15 13:36:58 -0500

    HomeScreen 0.1.0

--- Pokedex/src/main/HomeScreen.java
+++ Pokedex/src/main/HomeScreen.java
@@ -16,18 +16,21 @@ public class HomeScreen extends PokedexScreen {
 	public void initRemainingItems(ArrayList<Visible> viewObjects) {
 		int xShift = 0;
 		int yShift = 0;
-		for(int i = 0; i < appLinks.size(); i++){
-			if(i < 16){
-				if(i !=0 && i%4 == 0){
-				xShift = 0;
-				yShift += 75;
-			}else{
-				xShift += 75;
-			}
-			appLinks.get(i).setX(330+i*xShift);
-			appLinks.get(i).setY(90+i*yShift);
+		if(appLinks != null){
+			for(int i = 0; i < appLinks.size(); i++){
+				if(i < 16){
+					if(i !=0 && i%4 == 0){
+					xShift = 0;
+					yShift += 75;
+					}else{
+						xShift += 75;
+					}
+				appLinks.get(i).setX(330+i*xShift);
+				appLinks.get(i).setY(90+i*yShift);
+				}
 			}
 		}
+		
 	}
 
 }

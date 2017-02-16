19a69534c16e7b78a19fd1b31f289788fc2a7735
Student 8 <Student 8@10.8.33.101>
2017-02-16 13:15:01 -0500

    Joee

--- Pokedex/src/TwentyFortyEight/MyDemo.java
+++ Pokedex/src/TwentyFortyEight/MyDemo.java
@@ -32,15 +32,28 @@ public class MyDemo extends Component {
 		int[][]a = new int[4][4];
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setColor(Color.black);
-	
+		String pics[] = { "resources/images2048/grass.jpg" , "resources/images2048/bulbasaur.jpg",
+		"resources/images2048/ivysaur.jpg" }; // this is filler.. we don't have ivysaur sprite in images2048 yet
+		changeImages();
 		int height = 40,width = 40;
+		Graphic picture1 = new Graphic(50,50,5,"resources/images2048/grass.jpg");
+		Graphic picture2 = new Graphic(50,50,5,"resources/images2048/bulbasaur.jpg");
+		Graphic picture3 = new Graphic(50,50,5,"resources/images2048/ivysaur.jpg");
 		for (int row = 0; row < a.length; row++) {
 			for (int col = 0; col < a.length; col++) {
 				g.drawRect(col*width,row*height, width, height);
-				Graphic picture = new Graphic(50,50,5,"resources/images2048/grass.jpg");
-				g.drawImage(picture.getImage(), picture.getX(), picture.getY(), null);
-//
-//					
+				//this is just filler code
+				//there will be a double for loop instead of 11 if statements because 2^11 = 2048
+				if (board[row][col] == 0) {
+					g.drawImage(picture1.getImage(), picture1.getX(), picture1.getY(), null);
+				}
+				if (board[row][col] == 1) {
+					g.drawImage(picture2.getImage(), picture2.getX(), picture2.getY(), null);
+				}
+				if (board[row][col] == 2) {
+					g.drawImage(picture3.getImage(), picture3.getX(), picture3.getY(), null);
+				}
+				
 //				}
 			}
 		}
@@ -63,10 +76,7 @@ public class MyDemo extends Component {
 //		== 1 image is bulbasaur
 //				 == 2 is ivysaur
 	}
-	public void setNewImages(){
-		//don't know where to put this yet
-		// this will change the images based on the values (reason stated above)
-	}
+
 	
 	
 

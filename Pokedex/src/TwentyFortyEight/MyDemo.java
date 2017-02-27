1088c629dec5ab163cb95caeb0d7d7cee509aec3
Student 8 <Student 8@10.8.33.150>
2017-02-27 14:02:18 -0500

    asdasd

--- Pokedex/src/TwentyFortyEight/MyDemo.java
+++ Pokedex/src/TwentyFortyEight/MyDemo.java
@@ -30,52 +30,53 @@ public class MyDemo extends Component {
 	@Override
 	public void update(Graphics2D g) {
 		int[][]a = new int[4][4];
+		int x = 1;
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setColor(Color.black);
-		String pics[] = { "resources/images2048/grass.jpg" , "resources/images2048/bulbasaur.jpg",
+		String pics[] = { "resources/images2048/grass.jpg" , "resources/images2048/bulbasaur.png",
 		"resources/images2048/ivysaur.jpg" }; // this is filler.. we don't have ivysaur sprite in images2048 yet
-		changeImages();
-		int height = 40,width = 40;
-		Graphic picture1 = new Graphic(50,50,5,"resources/images2048/grass.jpg");
-		Graphic picture2 = new Graphic(50,50,5,"resources/images2048/bulbasaur.jpg");
-		Graphic picture3 = new Graphic(50,50,5,"resources/images2048/ivysaur.jpg");
+		//changeImages();
+		int height = 80,width = 80;
+//		Graphic picture1 = new Graphic(0,0,80,80,"resources/images2048/grass.jpg");
+//		Graphic picture2 = new Graphic(50,50,5,"resources/images2048/bulbasaur.jpg");
+//		Graphic picture3 = new Graphic(50,50,5,"resources/images2048/ivysaur.jpg");
 		for (int row = 0; row < a.length; row++) {
-			for (int col = 0; col < a.length; col++) {
-				g.drawRect(col*width,row*height, width, height);
-				//this is just filler code
-				//there will be a double for loop instead of 11 if statements because 2^11 = 2048
-				if (board[row][col] == 0) {
-					g.drawImage(picture1.getImage(), picture1.getX(), picture1.getY(), null);
-				}
-				if (board[row][col] == 1) {
-					g.drawImage(picture2.getImage(), picture2.getX(), picture2.getY(), null);
-				}
-				if (board[row][col] == 2) {
-					g.drawImage(picture3.getImage(), picture3.getX(), picture3.getY(), null);
-				}
+			for (int col = 0; col < a[row].length; col++) {
+				x = a[row][col];
 				
-//				}
+				Graphic picture2 = new Graphic(col*width,row*height,80,80,"resources/images2048/bulbasaur.png");
+				Graphic picture1 = new Graphic(col*width,row*height,80,80,"resources/images2048/grass.jpg");
+				
+				g.drawImage(picture1.getImage(), picture1.getX(), picture1.getY(), null);
+				g.drawRect(col*width,row*height, width, height);
+				
+				if(x == 0){
+					g.drawImage(picture2.getImage(), picture2.getX(), picture2.getY(), null);
+					//g. background image somehow
 			}
-		}
-		g.drawRect(120, 230, 170, 60);
+		}		g.drawRect(120, 230, 170, 60);
 		board = a;
 	
 		
 		
 	}
-	public void changeImages(){
-		// checks array values.
-		// if its 2 then it will be bulbasaur
-		// default (0) = grass img
-		for (int row = 0; row < board.length; row++) {
-			for (int col = 0; col < board[row].length; col++) {
-				changeBoard[row][col] = (int) Math.sqrt(board[row][col]);
- 			}
-		}
-//		// if values of chaneBoard[row][col] == 0  -> img is grass
-//		== 1 image is bulbasaur
-//				 == 2 is ivysaur
+	
+
+
 	}
+//	public void changeImages(){
+//		// checks array values.
+//		// if its 2 then it will be bulbasaur
+//		// default (0) = grass img
+//		for (int row = 0; row < board.length; row++) {
+//			for (int col = 0; col < board[row].length; col++) {
+//				changeBoard[row][col] = (int) Math.sqrt(board[row][col]);
+// 			}
+//		}
+////		// if values of chaneBoard[row][col] == 0  -> img is grass
+////		== 1 image is bulbasaur
+////				 == 2 is ivysaur
+//	}
 
 	
 	

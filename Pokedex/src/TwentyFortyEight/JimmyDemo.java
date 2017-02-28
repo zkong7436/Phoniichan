debf280d9d644751e70625e6fb00e70841ddbf98
XD <Jimmyyyy@users.noreply.github.com>
2017-02-28 14:02:55 -0500

    3243423

--- Pokedex/src/TwentyFortyEight/JimmyDemo.java
+++ Pokedex/src/TwentyFortyEight/JimmyDemo.java
@@ -6,6 +6,7 @@ package TwentyFortyEight;
 import java.awt.Color;
 import java.util.ArrayList;
 
+
 import guiPractice.GuiApplication;
 import guiPractice.components.Action;
 import guiPractice.components.Button;
@@ -18,6 +19,12 @@ import guiPractice.components.Visible;
  *
  */
 public class JimmyDemo extends GuiApplication {
+
+	static int[][] game;
+	static int temp;
+	
+	
+	
 	public static Reward reward;
 
 	/**
@@ -46,6 +53,33 @@ public class JimmyDemo extends GuiApplication {
 		JimmyDemo d = new JimmyDemo();
 		Thread app = new Thread(d);
 		app.start();
+		
+		game = new int[4][4];
+		int x = 0;
+		for (int i = 0; i < game.length; i++) {
+			for (int j = 0; j < game[i].length; j++) {
+				game[i][j] = x;
+
+			}
+		}
+		game[0][0] = 2;
+		game[0][1] = 2;
+		game[0][2] = 4;
+		game[0][3] = 64;
+		game[1][0] = 16;
+		game[1][1] = 32;
+		game[1][2] = 4;
+		game[1][3] = 2;
+		game[2][0] = 8;
+		game[2][1] = 2;
+		game[2][2] = 16;
+		game[2][3] = 2;
+		game[3][0] = 32;
+		game[3][1] = 2;
+		game[3][2] = 8;
+		game[3][3] = 2;
+
+		printGame(game);
 
 	}
 
@@ -69,7 +103,10 @@ public class JimmyDemo extends GuiApplication {
 
 				@Override
 				public void act() {
-					moveTilesUp();
+					rotate(game, 1);
+					moveTilesMinus(game);
+					rotate(game, 3);
+					printGame(game);
 					JimmyDemo.reward.getReward(10);
 					rewardDisplay.setText(" You earned a reward ! Total points = " + JimmyDemo.reward.getPoints());
 
@@ -79,6 +116,11 @@ public class JimmyDemo extends GuiApplication {
 
 				@Override
 				public void act() {
+					rotate(game, 1);
+					moveTilesPlus(game);
+					rotate(game, 3);
+					printGame(game);
+					
 					JimmyDemo.reward.getReward(10);
 					rewardDisplay.setText(" You earned a reward ! Total points = " + JimmyDemo.reward.getPoints());
 
@@ -88,6 +130,8 @@ public class JimmyDemo extends GuiApplication {
 
 				@Override
 				public void act() {
+					moveTilesMinus(game);
+					printGame(game);
 					JimmyDemo.reward.getReward(10);
 					rewardDisplay.setText(" You earned a reward ! Total points = " + JimmyDemo.reward.getPoints());
 
@@ -97,6 +141,8 @@ public class JimmyDemo extends GuiApplication {
 
 				@Override
 				public void act() {
+					moveTilesPlus(game);
+					printGame(game);
 					JimmyDemo.reward.getReward(10);
 					rewardDisplay.setText(" You earned a reward ! Total points = " + JimmyDemo.reward.getPoints());
 
@@ -111,20 +157,109 @@ public class JimmyDemo extends GuiApplication {
 
 	}
 
-	private static void moveTilesUp() {
-		
+	
+
+	public static void rotate(int[][] game, int numberOfRotations) {
+		// rotates clockwise
+		int[][] newArr = new int[4][4];
+		while (numberOfRotations >= 1) {
+			for (int i = 0; i < game.length; i++) {
+				for (int j = 0; j < game[i].length; j++) {
+					newArr[j][3 - i] = game[i][j];
+				}
+			}
+			for (int i = 0; i < game.length; i++) {
+				for (int j = 0; j < game[i].length; j++) {
+					game[i][j] = newArr[i][j];
+				}
+			}
+			numberOfRotations--;
+		}
+
 	}
 
-	private static void moveTilesDown() {
+	private static void combineTiles(int[] row) {
+		for (int k = 0; k < row.length - 1; k++) {
+			if (row[k] == row[k + 1]) {
+				row[k] = row[k] + row[k + 1];
+				row[k + 1] = 0;
+			}
+		}
+	}
+
+	public static void moveTilesPlus(int[][] game) {
+		for (int i = 0; i < game.length; i++) {
+			int[] row = new int[4];
+			for (int j = 0; j < game[i].length; j++) {
+				if (game[i][j] != 0) {
+					int temp = 0;
+					while (row[temp] != 0) {
+						temp++;
+					}
+					row[temp] = game[i][j];
+				}
+			}
+			combineTiles(row);
+
+			game[i] = row;
+		}
+		for (int i = 0; i < game.length; i++) {
+			int[] row = new int[4];
+			for (int j = 0; j < game[i].length; j++) {
+				if (game[i][j] != 0) {
+					int temp = 0;
+					while (row[temp] != 0) {
+						temp++;
+					}
+					row[temp] = game[i][j];
+				}
+			}
+			game[i] = row;
+		}
 
 	}
 
-	private static void moveTilesLeft() {
+	public static void moveTilesMinus(int[][] game) {
+		for (int i = 0; i < game.length; i++) {
+			int[] row = new int[4];
+			for (int j = 0; j < game[i].length; j++) {
+				if (game[i][j] != 0) {
+					int temp = 3;
+					while (row[temp] != 0) {
+						temp--;
+					}
+					row[temp] = game[i][j];
+				}
+			}
+			combineTiles(row);
+
+			game[i] = row;
+		}
+		for (int i = 0; i < game.length; i++) {
+			int[] row = new int[4];
+			for (int j = 0; j < game[i].length; j++) {
+				if (game[i][j] != 0) {
+					int temp = 3;
+					while (row[temp] != 0) {
+						temp--;
+					}
+					row[temp] = game[i][j];
+				}
+			}
+			game[i] = row;
+		}
 
 	}
 
-	private static void moveTilesRight() {
+	public static void printGame(int[][] game) {
+		for (int[] row : game) {
+			for (int i : row) {
+				System.out.print(i);
+				System.out.print("\t");
+			}
+			System.out.println();
 
+		}
 	}
 
 }

0750161cc074963ffddf145af41f506f839b93f9
zkong7436 <Wendigo@MSI>
2017-03-02 07:45:25 -0500

    Updated AppsLink. Started Pokedex app.

---
+++ Pokedex/src/main/NewPokemon.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package main;
+
+import java.util.ArrayList;
+
+import guiPractice.components.Visible;
+
+/**
+ * @author Wendigo
+ *
+ */
+public class NewPokemon extends PokedexScreen {
+
+	/**
+	 * @param width
+	 * @param height
+	 */
+	public NewPokemon(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see main.PokedexScreen#initRemainingItems(java.util.ArrayList)
+	 */
+	@Override
+	public void initRemainingItems(ArrayList<Visible> viewObjects) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Pokedex/src/main/Pokemon.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author Wendigo
+ *
+ */
+public class Pokemon {
+
+	/**
+	 * 
+	 */
+	private String name;
+	//more
+	
+	public Pokemon(String name) {
+		// TODO Auto-generated constructor stub
+		this.name=name;
+	}
+
+}

220d0f21340a61a1a800e2b0797b5c83b080a47c
XD <Jimmyyyy@users.noreply.github.com>
2017-03-03 08:07:59 -0500

    Update JimmyInterface.java

--- Pokedex/src/TwentyFortyEight/JimmyInterface.java
+++ Pokedex/src/TwentyFortyEight/JimmyInterface.java
@@ -1,10 +1,12 @@
 package TwentyFortyEight;
 
+import java.util.ArrayList;
+
 public interface JimmyInterface {
 	void printGame(int[][] game);
 	int counter(int number, int[][] gameTemp, int[][] game);
 	void createTemp(int[][] gameTemp, int[][]game);
 	void rotate(int[][] game, int numberOfRotations);
-	void moveTilesPlus(int[][] game);
-	void moveTilesMinus(int[][] game);
+	void moveTilesPlus(int[][] game, ArrayList<Integer> points);
+	void moveTilesMinus(int[][] game, ArrayList<Integer> points);
 }

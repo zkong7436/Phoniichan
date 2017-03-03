450e23f406f7ed41a29539be1d976ce2bc0f3569
XD <Jimmyyyy@users.noreply.github.com>
2017-03-03 00:32:45 -0500

    Update JimmyDemo.java

--- Pokedex/src/TwentyFortyEight/JimmyDemo.java
+++ Pokedex/src/TwentyFortyEight/JimmyDemo.java
@@ -20,174 +20,174 @@ import guiPractice.components.Visible;
  * @author Jimmy Wu
  *
  */
-public class JimmyDemo extends GuiApplication  {
-
-	static int[][] game;
-	static int[][] gameTemp;
- 	static int temp;
+public class JimmyDemo implements JimmyInterface  {
+	public static JimmyDemo jimmy = new JimmyDemo();
+//	static int[][] game;
+//	static int[][] gameTemp;
+// 	static int temp;
 	static ArrayList<Integer> points = new ArrayList();
-	
-	
-	public static Reward reward;
-
-	/**
-	 * 
-	 */
-	public JimmyDemo() {
-		reward = new Reward();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see guiPractice.GuiApplication#initScreen()
-	 */
-	@Override
-	protected void initScreen() {
-		DemoScreen demo = new DemoScreen(getWidth(), getHeight());
-		setScreen(demo);
-
-	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		JimmyDemo d = new JimmyDemo();
-		Thread app = new Thread(d);
-		app.start();
-		
-		game = new int[4][4];
-		gameTemp = new int[4][4];
-		SpawnDemo.start(game);
-		createTemp(gameTemp, game);
-		printGame(game);
-		System.out.println("+++++++++++++++++++++++++++++++");
-	
-	}
-
-	// nested inner class
-	private class DemoScreen extends ClickableScreen {
-		private TextLabel movementDisplay;
-		private Button slideUp;
-		private Button slideDown;
-		private Button slideLeft;
-		private Button slideRight;
-
-		public DemoScreen(int width, int height) {
-			super(width, height);
-			// TODO Auto-generated constructor stub
-		}
-
-		@Override
-		public void initAllObjects(ArrayList<Visible> view) {
-			movementDisplay = new TextLabel(20, 40, 1000, 25, "");
-			slideUp = new Button(100, 75, 90, 40, "Up", Color.blue, new Action() {
-
-				@Override
-				public void act() {
-					createTemp(gameTemp, game);
-					rotate(game, 1);
-					moveTilesMinus(game);
-					rotate(game, 3);
-					int counter = counter(16, gameTemp, game);
-					
-					if (counter!=0) {
-						SpawnDemo.cont(game);
-						printGame(game);
-						
-						System.out.println("-------------------------------");
-						System.out.println(Arrays.toString(points.toArray()));
-						movementDisplay.setText("You swiped up!");
-					}else{
-						movementDisplay.setText("You cannot swipe up anymore.");
-					}
-					
-
-				}
-			});
-			slideDown = new Button(100, 225, 90, 40, "Down", Color.blue, new Action() {
-
-				@Override
-				public void act() {
-					createTemp(gameTemp, game);
-					rotate(game, 1);
-					moveTilesPlus(game);
-					rotate(game, 3);
-					int counter = counter(16, gameTemp, game);
-					
-					if (counter!=0) {
-						SpawnDemo.cont(game);
-						printGame(game);
-						
-						System.out.println("-------------------------------");
-						System.out.println(Arrays.toString(points.toArray()));
-						movementDisplay.setText("You swiped down!");
-					}else{
-						movementDisplay.setText("You cannot swipe down anymore.");
-					}
-					
-					
-
-				}
-			});
-			slideRight = new Button(160, 150, 90, 40, "Right", Color.blue, new Action() {
-
-				@Override
-				public void act() {
-					createTemp(gameTemp, game);
-					moveTilesMinus(game);		
-					int counter = counter(16, gameTemp, game);
-					
-					if (counter != 0) {
-						SpawnDemo.cont(game);
-						printGame(game);
-						System.out.println("-------------------------------");
-						System.out.println(Arrays.toString(points.toArray()));
-						movementDisplay.setText("You swiped right!");
-					}else {
-						movementDisplay.setText("You cannot swipe right anymore. ");
-					}
-					
-
-				}
-			});
-			slideLeft = new Button(40, 150, 90, 40, "Left", Color.blue, new Action() {
-
-				@Override
-				public void act() {
-					createTemp(gameTemp, game);
-					moveTilesPlus(game);
-					int counter = counter(16, gameTemp, game);
-					
-					if (counter != 0) {
-						SpawnDemo.cont(game);
-						printGame(game);
-						System.out.println("-----------------------------"
-								+ "");
-						System.out.println(Arrays.toString(points.toArray()));
-						movementDisplay.setText("You swiped left!");
-					}else {
-						movementDisplay.setText("You cannot swipe left anymore. ");
-					}
-					
-					
-					
-					
-					
-					
-				}
-			});
-			view.add(movementDisplay);
-			view.add(slideUp);
-			view.add(slideDown);
-			view.add(slideRight);
-			view.add(slideLeft);
-		}
-
-	}
-	
-	public static int counter(int number, int[][] gameTemp, int[][] game) {
+//	
+//	
+//	public static Reward reward;
+//
+//	/**
+//	 * 
+//	 */
+//	public JimmyDemo() {
+//		jimmy= this;
+//	}
+//
+//	/*
+//	 * (non-Javadoc)
+//	 * 
+//	 * @see guiPractice.GuiApplication#initScreen()
+//	 */
+//	@Override
+//	protected void initScreen() {
+//		DemoScreen demo = new DemoScreen(getWidth(), getHeight());
+//		setScreen(demo);
+//
+//	}
+//
+//	/**
+//	 * @param args
+//	 */
+//	public static void main(String[] args) {
+//		JimmyDemo d = new JimmyDemo();
+//		Thread app = new Thread(d);
+//		app.start();
+//		
+//		game = new int[4][4];
+//		gameTemp = new int[4][4];
+//		SpawnDemo.start(game);
+//		createTemp(gameTemp, game);
+//		printGame(game);
+//		System.out.println("+++++++++++++++++++++++++++++++");
+//	
+//	}
+//
+//	// nested inner class
+//	private class DemoScreen extends ClickableScreen {
+//		private TextLabel movementDisplay;
+//		private Button slideUp;
+//		private Button slideDown;
+//		private Button slideLeft;
+//		private Button slideRight;
+//
+//		public DemoScreen(int width, int height) {
+//			super(width, height);
+//			// TODO Auto-generated constructor stub
+//		}
+//
+//		@Override
+//		public void initAllObjects(ArrayList<Visible> view) {
+//			movementDisplay = new TextLabel(20, 40, 1000, 25, "");
+//			slideUp = new Button(100, 75, 90, 40, "Up", Color.blue, new Action() {
+//
+//				@Override
+//				public void act() {
+//					createTemp(gameTemp, game);
+//					rotate(game, 1);
+//					moveTilesMinus(game);
+//					rotate(game, 3);
+//					int counter = counter(16, gameTemp, game);
+//					
+//					if (counter!=0) {
+//						SpawnDemo.cont(game);
+//						printGame(game);
+//						
+//						System.out.println("-------------------------------");
+//						System.out.println(Arrays.toString(points.toArray()));
+//						movementDisplay.setText("You swiped up!");
+//					}else{
+//						movementDisplay.setText("You cannot swipe up anymore.");
+//					}
+//					
+//
+//				}
+//			});
+//			slideDown = new Button(100, 225, 90, 40, "Down", Color.blue, new Action() {
+//
+//				@Override
+//				public void act() {
+//					createTemp(gameTemp, game);
+//					rotate(game, 1);
+//					moveTilesPlus(game);
+//					rotate(game, 3);
+//					int counter = counter(16, gameTemp, game);
+//					
+//					if (counter!=0) {
+//						SpawnDemo.cont(game);
+//						printGame(game);
+//						
+//						System.out.println("-------------------------------");
+//						System.out.println(Arrays.toString(points.toArray()));
+//						movementDisplay.setText("You swiped down!");
+//					}else{
+//						movementDisplay.setText("You cannot swipe down anymore.");
+//					}
+//					
+//					
+//
+//				}
+//			});
+//			slideRight = new Button(160, 150, 90, 40, "Right", Color.blue, new Action() {
+//
+//				@Override
+//				public void act() {
+//					createTemp(gameTemp, game);
+//					moveTilesMinus(game);		
+//					int counter = counter(16, gameTemp, game);
+//					
+//					if (counter != 0) {
+//						SpawnDemo.cont(game);
+//						printGame(game);
+//						System.out.println("-------------------------------");
+//						System.out.println(Arrays.toString(points.toArray()));
+//						movementDisplay.setText("You swiped right!");
+//					}else {
+//						movementDisplay.setText("You cannot swipe right anymore. ");
+//					}
+//					
+//
+//				}
+//			});
+//			slideLeft = new Button(40, 150, 90, 40, "Left", Color.blue, new Action() {
+//
+//				@Override
+//				public void act() {
+//					createTemp(gameTemp, game);
+//					moveTilesPlus(game);
+//					int counter = counter(16, gameTemp, game);
+//					
+//					if (counter != 0) {
+//						SpawnDemo.cont(game);
+//						printGame(game);
+//						System.out.println("-----------------------------"
+//								+ "");
+//						System.out.println(Arrays.toString(points.toArray()));
+//						movementDisplay.setText("You swiped left!");
+//					}else {
+//						movementDisplay.setText("You cannot swipe left anymore. ");
+//					}
+//					
+//					
+//					
+//					
+//					
+//					
+//				}
+//			});
+//			view.add(movementDisplay);
+//			view.add(slideUp);
+//			view.add(slideDown);
+//			view.add(slideRight);
+//			view.add(slideLeft);
+//		}
+//
+//	}
+//	
+	public int counter(int number, int[][] gameTemp, int[][] game) {
 		for (int i = 0; i < game.length; i++) {
 			for (int j = 0; j < game[i].length; j++) {
 				if (gameTemp[i][j] == game[i][j]) {
@@ -198,7 +198,7 @@ public class JimmyDemo extends GuiApplication  {
 		return number;
 	
 	}
-	public static void createTemp(int[][] gameTemp, int[][]game) {
+	public void createTemp(int[][] gameTemp, int[][]game) {
 		
 		for (int i = 0; i < gameTemp.length; i++) {
 			for (int j = 0; j < gameTemp[i].length; j++) {
@@ -209,7 +209,7 @@ public class JimmyDemo extends GuiApplication  {
 	}
 
 
-	public static void rotate(int[][] game, int numberOfRotations) {
+	public void rotate(int[][] game, int numberOfRotations) {
 		// rotates clockwise
 		int[][] newArr = new int[4][4];
 		while (numberOfRotations >= 1) {
@@ -244,7 +244,7 @@ public class JimmyDemo extends GuiApplication  {
 		
 	}
 
-	public static void moveTilesPlus(int[][] game) {
+	public void moveTilesPlus(int[][] game) {
 		for (int i = 0; i < game.length; i++) {
 			int[] row = new int[4];
 			for (int j = 0; j < game[i].length; j++) {
@@ -276,7 +276,7 @@ public class JimmyDemo extends GuiApplication  {
 
 	}
 
-	public static void moveTilesMinus(int[][] game) {
+	public void moveTilesMinus(int[][] game) {
 		for (int i = 0; i < game.length; i++) {
 			int[] row = new int[4];
 			for (int j = 0; j < game[i].length; j++) {
@@ -310,7 +310,7 @@ public class JimmyDemo extends GuiApplication  {
 		
 	
 	
-	public static void printGame(int[][] game) {
+	public void printGame(int[][] game) {
 		for (int[] row : game) {
 			for (int i : row) {
 				System.out.print(i);

14e9667c58e872390fe3a9d378bae84f53768376
XD <Jimmyyyy@users.noreply.github.com>
2017-03-03 07:17:21 -0500

    Add files via upload

---
+++ Pokedex/src/main/JosephDemo.java
@@ -0,0 +1,208 @@
+/**
+ * 
+ */
+package TwentyFortyEight;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import guiPractice.GuiApplication;
+import guiPractice.components.Action;
+import guiPractice.components.Button;
+import guiPractice.components.ClickableScreen;
+import guiPractice.components.Graphic;
+import guiPractice.components.TextLabel;
+import guiPractice.components.Visible;
+// adding ponts into a arraylist
+
+/**
+ * @author Jimmy Wu
+ *
+ */
+public class JosephDemo extends GuiApplication  {
+
+	static int[][] gameTemp;
+ 	static int temp;
+	static ArrayList<Integer> points = new ArrayList();
+	
+	
+	public static Reward reward;
+
+	/**
+	 * 
+	 */
+	public JosephDemo() {
+		reward = new Reward();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see guiPractice.GuiApplication#initScreen()
+	 */
+	@Override
+	protected void initScreen() {
+		DemoScreen demo = new DemoScreen(1000, 1000);
+		setScreen(demo);
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JosephDemo d = new JosephDemo();
+		Thread app = new Thread(d);
+		app.start();
+		
+		
+		gameTemp = new int[4][4];
+		JimmyDemo.jimmy.createTemp(gameTemp, MyDemo.game);
+	
+		JimmyDemo.jimmy.printGame(MyDemo.game);
+		System.out.println("+++++++++++++++++++++++++++++++");
+	
+	}
+
+	// nested inner class
+	private class DemoScreen  extends main.PokedexScreen {
+		private TextLabel movementDisplay;
+		private Button slideUp;
+		private Button slideDown;
+		private Button slideLeft;
+		private Button slideRight;
+		
+
+		public DemoScreen(int width, int height) {
+			super(width, height);
+			// TODO Auto-generated constructor stub
+		}
+
+
+		@Override
+		public void initRemainingItems(ArrayList<Visible> view) {
+				movementDisplay = new TextLabel(20, 40, 1000, 25, "");
+				slideUp = new Button(785, 320, 40, 40, "Up", Color.green, new Action() {
+
+					@Override
+					public void act() {
+						JimmyDemo.jimmy.createTemp(gameTemp, MyDemo.game);
+						JimmyDemo.jimmy.rotate(MyDemo.game, 1);
+						JimmyDemo.jimmy.moveTilesMinus(MyDemo.game);
+						JimmyDemo.jimmy.rotate(MyDemo.game, 3);
+						int counter = JimmyDemo.jimmy.counter(16, gameTemp, MyDemo.game);
+						
+						if (counter!=0) {
+							SpawnDemo.spawn.cont(MyDemo.game);
+							JimmyDemo.jimmy.printGame(MyDemo.game);
+							
+							
+							System.out.println("-------------------------------");
+							System.out.println(Arrays.toString(points.toArray()));
+							movementDisplay.setText("You swiped up!");
+						}else{
+							movementDisplay.setText("You cannot swipe up anymore.");
+						}
+						
+
+					}
+				});
+				slideDown = new Button(785, 390, 40, 40, "Down", Color.green, new Action() {
+
+					@Override
+					public void act() {
+						JimmyDemo.jimmy.createTemp(gameTemp, MyDemo.game);
+						JimmyDemo.jimmy.rotate(MyDemo.game, 1);
+						JimmyDemo.jimmy.moveTilesPlus(MyDemo.game);
+						JimmyDemo.jimmy.rotate(MyDemo.game, 3);
+						int counter = JimmyDemo.jimmy.counter(16, gameTemp, MyDemo.game);
+						
+						if (counter!=0) {
+							SpawnDemo.spawn.cont(MyDemo.game);
+							JimmyDemo.jimmy.printGame(MyDemo.game);
+							
+							System.out.println("-------------------------------");
+							System.out.println(Arrays.toString(points.toArray()));
+							movementDisplay.setText("You swiped down!");
+							update();
+						}else{
+							movementDisplay.setText("You cannot swipe down anymore.");
+						}
+						
+						
+
+					}
+				});
+				slideRight = new Button(827, 355, 40, 40, "Right", Color.green, new Action() {
+
+					@Override
+					public void act() {
+						JimmyDemo.jimmy.createTemp(gameTemp, MyDemo.game);
+						JimmyDemo.jimmy.moveTilesMinus(MyDemo.game);		
+						int counter = JimmyDemo.jimmy.counter(16, gameTemp, MyDemo.game);
+						if (counter != 0) {
+							SpawnDemo.spawn.cont(MyDemo.game);
+							JimmyDemo.jimmy.printGame(MyDemo.game);
+							System.out.println("-------------------------------");
+							System.out.println(Arrays.toString(points.toArray()));
+							movementDisplay.setText("You swiped right!");
+						}else {
+							movementDisplay.setText("You cannot swipe right anymore. ");
+						}
+						
+
+					}
+				});
+				slideLeft = new Button(740, 355, 40, 40, "Left", Color.green, new Action() {
+
+					@Override
+					public void act() {
+						JimmyDemo.jimmy.createTemp(gameTemp, MyDemo.game);
+						JimmyDemo.jimmy.moveTilesPlus(MyDemo.game);
+						int counter = JimmyDemo.jimmy.counter(16, gameTemp, MyDemo.game);
+						
+						if (counter != 0) {
+							SpawnDemo.spawn.cont(MyDemo.game);
+							JimmyDemo.jimmy.printGame(MyDemo.game);
+							System.out.println("-----------------------------"
+									+ "");
+							System.out.println(Arrays.toString(points.toArray()));
+							movementDisplay.setText("You swiped left!");
+						}else {
+							movementDisplay.setText("You cannot swipe left anymore. ");
+						}	
+					}
+				});
+				view.add(movementDisplay);
+				view.add(slideUp);
+				view.add(slideDown);
+				view.add(slideRight);
+				view.add(slideLeft);
+				MyDemo d = new MyDemo(330,250,380,345);
+				TextLabel text = new TextLabel(560, 220, 100, 30, "High Score");
+				int[] scores = new int[3];
+		//		if(TwentyFortyEightGame.gameOver){
+		//			highScore(score, scores);
+		//		}
+				for(int i = 0; i < 3; i++){
+					
+					scores[0] = 1;
+					scores[1] = 2;
+					scores[2] = 3;
+					view.add(new TextLabel((560) , (230 + (i*20)), 30, 40, scores[i]+""));			
+			}
+				view.add(d);
+				view.add(text);
+			}
+	
+		}
+
+	}
+	
+	
+
+
---
+++ Pokedex/src/main/MyDemo.java
@@ -0,0 +1,107 @@
+package TwentyFortyEight;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import guiPractice.components.Component;
+import guiPractice.components.Graphic;
+
+/**
+ * @author Student 8
+ *
+ */
+public class MyDemo extends Component {
+	static int[][]game = new int[4][4];
+	public MyDemo(int x, int y, int w, int h) {
+		super(x, y, w, h);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+
+	@Override
+	public void update(Graphics2D g) {
+//		a[0][0] = 2;
+//		a[0][3] = 2;
+		SpawnDemo.spawn.start(game);
+		int y = 1;
+		int z = 1;
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g.setColor(Color.black);
+		String pics[] = { "resources/images2048/grass.jpg" , "resources/images2048/1.png",
+		"resources/images2048/ivysaur.png", "resources/images2048/venasaur.png", "resources/images2048/4.png",
+		"resources/images2048/5.png", "resources/images2048/6.png", "resources/images2048/7.png",
+		"resources/images2048/8.png", "resources/images2048/9.png", "resources/images2048/10.png",
+		"resources/images2048/11.png", "resources/images2048/12.png", "resources/images2048/13.png",
+		"resources/images2048/14.png", "resources/images2048/15.png",}; // this is filler.. we don't have ivysaur sprite in images2048 yet
+		//changeImages();
+		int height = 55,width = 55;
+//		Graphic picture1 = new Graphic(0,0,80,80,"resources/images2048/grass.jpg");
+
+		for (int row = 0; row < game.length; row++) {
+			for (int col = 0; col < game[row].length; col++) {
+				y = game[row][col];
+					if( y> 0){
+						 z= (int) (Math.log(y)/Math.log(2));
+					}
+					else{
+						z = 0;
+					}
+				 
+					Graphic picture2 = new Graphic(col*width,row*height,width,height,pics[z]);
+					g.drawImage(picture2.getImage(), picture2.getX(), picture2.getY(), null);
+					g.drawRect(col*width,row*height, width, height);
+		
+		}	
+		}
+	}
+}	
+
+
+	
+//	public void changeImages(){
+//		// checks array values.
+//		// if its 2 then it will be bulbasaur
+//		// default (0) = grass img
+//		for (int row = 0; row < board.length; row++) {
+//			for (int col = 0; col < board[row].length; col++) {
+//				changeBoard[row][col] = (int) Math.sqrt(board[row][col]);
+// 			}
+//		}
+////		// if values of chaneBoard[row][col] == 0  -> img is grass
+////		== 1 image is bulbasaur
+////				 == 2 is ivysaur
+//	}
+
+	
+	
+
+//	public void start(){
+//		ArrayList<Integer> Nrow = new ArrayList<Integer>();
+//		ArrayList<Integer> Ncol = new ArrayList<Integer>();
+//		for (int row = 0; row < board.length; row++) {
+//			for (int col= 0; col < board.length; col++) {
+//				if(board[row][col] == 0){
+//					Nrow.add(row);
+//					Ncol.add(col);
+//					//will add row and col of all unfilled squares
+//				}
+//			}
+//			for (int i = 0; i < 2; i++) {
+//				int rndX = (int)(Nrow.size()*Math.random());
+//				int rndY = (int)(Ncol.size()*Math.random());
+//				int expo = (int) ((1 * Math.random()) + 1); // (0 or 1) + 1;
+//				// 
+//				// 2 ^1 = 2; 2^2 = 4;  it will random 
+//				board[rndX][rndY] = (int) Math.pow(2, expo);
+//				Nrow.remove(rndX);
+//				Nrow.remove(rndY);
+//				
+//			}
+//		}
+//		
+//	}
+	
+

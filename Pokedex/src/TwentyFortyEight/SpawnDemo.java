97d79e2452029beeefed2a278f0d047c383c85c9
L0hL <izhen7813@bths.edu>
2017-03-03 01:07:14 -0500

    Update SpawnDemo.java

--- Pokedex/src/TwentyFortyEight/SpawnDemo.java
+++ Pokedex/src/TwentyFortyEight/SpawnDemo.java
@@ -3,38 +3,45 @@ package TwentyFortyEight;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class SpawnDemo implements SInterface{
+public  class SpawnDemo implements SpawnInterface{
 	private boolean gameOver = false;
 	private boolean move = false;
 	private static int [][]board = new int [4][4];
 	static ArrayList<Integer> Nrow = new ArrayList<Integer>();
 	static ArrayList<Integer> Ncol = new ArrayList<Integer>();
-	
+	public static SpawnDemo spawn = new SpawnDemo();
 	public static void main(String[] args) {
-		System.out.println("Game starts and two random blank blocks are turned into 2s or 4s" );
-		start(board);
-		System.out.println("After you move (which is not shown), a new blank block is turned into a 2 or 4"+"");
-		
-		
-		
-		
 
 	}
-	public static void checkBlanks(int[][]board){
-		for (int row = 0; row < board.length; row++) {
-			for (int col= 0; col < board.length; col++) {
-				if(board[row][col] == 0){
-					Nrow.add(row);
-					Ncol.add(col);
-					//will add row and col of all unfilled squares
-				}
-			}
-				
-			}
+	
+	public static void checkStatus(){
+//		for (int row = 0; row < board.length; row++) {
+//			for (int col = 0; col < board.length; col++) {
+//				if (board[row][col] != 0) {
+//					
+//				}
+//			}
+//		}
+//		if (condition) {
+//			
+//		}
 	}
-
-	public static void cont(int[][]board){
-		checkBlanks(board);
+	@Override
+	public void start(int[][]board) {
+		for(int i = 0; i<2; i++){
+			checkBlank(board);
+			int newExpo = (int)Math.pow(2, (int) (((2 * Math.random())) + 1));
+			int newRndX = (int)(Nrow.size()*Math.random()) ;
+			int newRndY = newRndX;
+			board[Nrow.get(newRndX)][Ncol.get(newRndY)] = newExpo;
+			Nrow.clear();
+			Ncol.clear();
+		}
+			
+	}
+	@Override
+	public void cont(int[][]board) {
+		checkBlank(board);
 		for(int i = 0; i<1; i++){
 			int newNum = 0;
 			
@@ -55,37 +62,17 @@ public class SpawnDemo implements SInterface{
 		Ncol.clear();
 		
 	}
-	
-	
-
-	public static void start(int [][]board){
-	for(int i = 0; i<2; i++){
-		checkBlanks(board);
-		int newExpo = (int)Math.pow(2, (int) (((2 * Math.random())) + 1));
-		int newRndX = (int)(Nrow.size()*Math.random()) ;
-		int newRndY = newRndX;
-		board[Nrow.get(newRndX)][Ncol.get(newRndY)] = newExpo;
-		Nrow.clear();
-		Ncol.clear();
-		
-	}
-	
-	}
-	public static void checkStatus(){
-//		for (int row = 0; row < board.length; row++) {
-//			for (int col = 0; col < board.length; col++) {
-//				if (board[row][col] != 0) {
-//					
-//				}
-//			}
-//		}
-//		if (condition) {
-//			
-//		}
-	}
-	@Override
-	public boolean gameOver() {
-		return gameOver = true;
+	public void checkBlank(int[][]board) {
+		for (int row = 0; row < board.length; row++) {
+			for (int col= 0; col < board.length; col++) {
+				if(board[row][col] == 0){
+					Nrow.add(row);
+					Ncol.add(col);
+					//will add row and col of all unfilled squares
+				}
+			}
+				
+			}
 		
 	}
 

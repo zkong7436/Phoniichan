6f71a9b8dd7f7572e78ed673f36fa1ff486c562e
zkong7436 <Wendigo@MSI>
2017-03-02 23:04:48 -0500

    added images

--- Pokedex/src/pokedex/PokedexList.java
+++ Pokedex/src/pokedex/PokedexList.java
@@ -3,11 +3,14 @@
  */
 package pokedex;
 
+import java.io.File;
 import java.util.ArrayList;
 
+import guiPractice.components.Action;
+import guiPractice.components.ClickableGraphic;
+import guiPractice.components.TextLabel;
 import guiPractice.components.Visible;
 import main.PokedexScreen;
-import main.Pokemon;
 
 /**
  * @author Wendigo
@@ -19,7 +22,7 @@ public class PokedexList extends PokedexScreen {
 	 * @param width
 	 * @param height
 	 */
-	private ArrayList<Pokemon> pokeList;
+	private ArrayList<ClickableGraphic> pokeList;
 	
 	public PokedexList(int width, int height) {
 		super(width, height);
@@ -31,9 +34,38 @@ public class PokedexList extends PokedexScreen {
 	 */
 	@Override
 	public void initRemainingItems(ArrayList<Visible> viewObjects) {
-		pokeList = new ArrayList<Pokemon>();
+		pokeList = new ArrayList<ClickableGraphic>();
 		viewObjects.addAll(pokeList);
-		update();
+	}
+	
+	public void findPokemon() {
+		File[] resources = new File("resources/pokemon").listFiles();
+		int numIcons = 0;
+		for(int i = 0; i < resources.length; i++){
+			ClickableGraphic poke = new ClickableGraphic((iconX(numIcons))*60+330,iconY(numIcons)*60+90,60,60,"resources/pokemon"+resources[i].getName());
+			poke.setAction(new Action(){
+
+				@Override
+				public void act() {
+					//goto application
+					
+				}
+				
+			});
+			String name = cutString(resources[i].getName(), "icon.png");
+			TextLabel newLinkLabel = new TextLabel((iconX(numIcons))*120+360,iconY(numIcons)*120+210,90,40,centerString(name, 8));
+			newLinkLabel.setSize(20);
+			numIcons++;
+			appLinks.add(newLink);
+		}
+	}
+	
+	public int iconX(int numIcons) {
+		return (numIcons%3);
+	}
+
+	public int iconY(int numIcons) {
+		return (numIcons/3);
 	}
 
 }
